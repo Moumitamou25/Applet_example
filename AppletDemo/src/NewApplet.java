3fcfd3db74ca26384864fb14102b4b1d65342ea3
unknown <Moumitachy21@gmail.com>
2021-06-06 00:35:08 +0600

    Applet_example

---
+++ AppletDemo/src/NewApplet.java
@@ -0,0 +1,18 @@
+
+
+import java.applet.Applet;
+import java.awt.*;
+
+
+public class NewApplet extends Applet {
+    
+        public void paint(Graphics g){
+               g.drawString("Object Oriented Programming",90,90);
+
+            }
+}
+        
+
+
+    
+
